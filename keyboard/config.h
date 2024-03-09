45f77766139ce2536e9903ce9d83339e81c7172d
Mario Medina <mariomedinalucero29@gmail.com>
2024-03-09 12:43:21 -0600

    mi configuracion personal

---
+++ keyboard/config.h
@@ -0,0 +1,73 @@
+
+
+#pragma once
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+//#define EE_HANDS
+
+#define QUICK_TAP_TERM 0
+//#define TAPPING_TERM 175
+#define TAPPING_TERM 200
+
+#define RETRO_TAPPING
+//#define IGNORE_MOD_TAP_INTERRUPT
+
+#ifdef RGBLIGHT_ENABLE
+#    define RGBLIGHT_BACKLIGHT_PIN {1,1}
+#    define BACKLIGHT_PIN {1,1}
+#    define RGBLIGHT_EFFECT_BREATHING
+#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+//#    define RGBLIGHT_EFFECT_SNAKE
+//#    define RGBLIGHT_EFFECT_KNIGHT
+#    define RGBLIGHT_EFFECT_CHRISTMAS
+#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#    define RGBLIGHT_EFFECT_RGB_TEST
+//#    define RGBLIGHT_EFFECT_ALTERNATING
+//#    define RGBLIGHT_EFFECT_TWINKLE
+#    define RGBLED_SPLIT {6,6}
+#    define RGBLIGHT_HUE_STEP 6 // number of steps to cycle through the hue by
+#    define RGBLIGHT_SAT_STEP 6 // number of steps to increment the saturation by
+#    define RGBLIGHT_VAL_STEP 6 // number of steps to increment the brightness by
+#    define RGBLIGHT_SLEEP      //  the RGB lighting will be switched off when the host goes to sleep
+#endif
+
+
+// #undef RGBLIGHT_LED_COUNT
+// #define RGBLIGHT_EFFECT_BREATHING
+// #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+// #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+// #define RGBLIGHT_EFFECT_SNAKE
+// #define RGBLIGHT_EFFECT_KNIGHT
+// #define RGBLIGHT_EFFECT_CHRISTMAS
+// #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+// #define RGBLIGHT_EFFECT_RGB_TEST
+// #define RGBLIGHT_EFFECT_ALTERNATING
+// #define RGBLIGHT_EFFECT_TWINKLE
+// //#define RGBLIGHT_LED_COUNT 27
+// #define RGBLIGHT_LIMIT_VAL 120
+// #define RGBLIGHT_HUE_STEP 10
+// #define RGBLIGHT_SAT_STEP 17
+// #define RGBLIGHT_VAL_STEP 17
+
+//#define RGBLED_NUM 1
+
+
+// Underglow
+
+
+//#undef  RGBLIGHT_LED_COUNT
+//#define RGBLIGHT_LED_COUNT 12    // Number of LEDs
+//#define RGBLIGHT_SLEEP
+
+
+
+// Underglow
+/*
+#undef RGBLIGHT_LED_COUNT
+#define RGBLIGHT_LED_COUNT 14    // Number of LEDs
+#define RGBLIGHT_SLEEP
+*/
