b642f73127bf0411e8c16e2802ea098664c71dba
Mario Medina <mariomedinalucero29@gmail.com>
2024-03-10 15:13:05 -0600

    alt left

--- keyboard/keymap.c
+++ keyboard/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD(TD_BAJO),
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, TD(TD_PUNTOS), TD(TD_COMASD),
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, TD(TD_LLAVEI), TD(TD_LLAVEC),  KC_N,    KC_M,    TD(TD_MENOR), TD(TD_MAYOR),  TD(TD_SIGNO), TD(TD_PIPA),
-                        KC_CAPS, KC_LGUI, MO(_LOWER), KC_ENT,   KC_SPC, MO(_RAISE),TD(TD_MAS), KC_RALT
+                         MT(MOD_LALT,KC_CAPS), KC_LGUI, MO(_LOWER), KC_ENT,   KC_SPC, MO(_RAISE),TD(TD_MAS), KC_RALT
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
