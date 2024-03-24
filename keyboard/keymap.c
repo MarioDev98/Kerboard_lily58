9df1c0c0c74abb3e5dae0116fed70955731af467
Mario Medina <mariomedinalucero29@gmail.com>
2024-03-24 11:10:19 -0600

    cambios letra control

--- keyboard/keymap.c
+++ keyboard/keymap.c
@@ -41,9 +41,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   QK_GESC,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    TD(TD_BAJO),
-  KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, TD(TD_PUNTOS), TD(TD_COMASD),
+  MT(MOD_LSFT, KC_CAPS),  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,          KC_H,    KC_J,    KC_K,    KC_L, TD(TD_PUNTOS), TD(TD_COMASD),
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, TD(TD_LLAVEI), TD(TD_LLAVEC),  KC_N,    KC_M,    TD(TD_MENOR), TD(TD_MAYOR),  TD(TD_SIGNO), TD(TD_PIPA),
-                         MT(MOD_LALT,KC_CAPS), KC_LGUI, MO(_LOWER), KC_ENT,   KC_SPC, MO(_RAISE),TD(TD_MAS), KC_RALT
+                       KC_LALT, KC_LGUI, MO(_LOWER), KC_ENT,   KC_SPC, MO(_RAISE),TD(TD_MAS), KC_RALT
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -123,6 +123,7 @@ tap_dance_action_t tap_dance_actions[] = {
   [TD_LLAVEI] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, LSFT(KC_LCBR)),
   [TD_LLAVEC] = ACTION_TAP_DANCE_DOUBLE(KC_RBRC, LSFT(KC_RCBR)),
   [TD_PUNTOS] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, LSFT(KC_COLN)),
+  
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
